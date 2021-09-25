7b211a970e04100ca7b3223a940ec259a0421938
myassinment <musakamama3838@gmail.com>
2021-09-25 15:02:48 +0900

    課題０です。

---
+++ asssinment.py
@@ -0,0 +1,17 @@
+name1 = "ねずこ"
+name2 = "ぜんいつ"
+list = ["たんじろう","ぎゆう","ねずこ","むざん"]
+
+# print(f"{name1}と{name2}は仲間です")
+# list.append("ぜんいつ")
+# print(list)
+
+# if name2 == "むざん":
+#     print(f"{name1}と{name2}は仲間ではありません")
+
+def forpractice(list):
+        print(f"{list}はリストに含まれています")    
+
+forpractice("たんじろう")
+
+
